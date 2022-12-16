cda7173d99f922fb5564b6b3ad4e8630907cae89
WhalesBob <ndw8200@naver.com>
2022-12-16 16:05:36 +0900

    feat : ProgramOutput 에, 페어 매칭 과정,레벨,미션 보여주는 기능 추가

--- src/main/java/pairmatching/views/ProgramOutput.java
+++ src/main/java/pairmatching/views/ProgramOutput.java
@@ -12,5 +12,33 @@ public class ProgramOutput {
         System.out.print("1. 페어 매칭\n2. 페어 조회\n3.페어 초기화\nQ. 종료\n");
     }
 
-    
+    public void showCourseAndMission(){
+        System.out.println("#############################################");
+        System.out.printf("과정 : %s | %s", Course.getName(Course.BACKEND),Course.getName(Course.FRONTEND));
+        System.out.println("미션:");
+        showMissions();
+        System.out.println("#############################################");
+        System.out.println("과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주");
+    }
+
+    public void showMissions(){
+        System.out.printf("  - 레벨1: %s\n",makeLevelMissionsList(Level.LEVEL1));
+        System.out.printf("  - 레벨2: %s\n",makeLevelMissionsList(Level.LEVEL2));
+        System.out.printf("  - 레벨3: %s\n",makeLevelMissionsList(Level.LEVEL3));
+        System.out.printf("  - 레벨4: %s\n",makeLevelMissionsList(Level.LEVEL4));
+        System.out.printf("  - 레벨5: %s\n",makeLevelMissionsList(Level.LEVEL5));
+    }
+
+    private String makeLevelMissionsList(Level level){
+        List<String> missions = Level.getMissions(level);
+        StringBuilder missionListOutput = new StringBuilder();
+        int missionSize = missions.size();
+        for(int i = 0; i < missionSize; i++){
+            missionListOutput.append(missions.get(i));
+            if(i < missionSize - 1){
+                missionListOutput.append(" | ");
+            }
+        }
+        return missionListOutput.toString();
+    }
 }
