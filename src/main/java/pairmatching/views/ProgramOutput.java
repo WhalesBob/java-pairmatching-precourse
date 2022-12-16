9b516c27e30790d3dc666a28e61f0cbcc99040d7
WhalesBob <ndw8200@naver.com>
2022-12-16 16:04:40 +0900

    feat : ProgramOutput 생성 후, 초기 화면 출력기능 추가

---
+++ src/main/java/pairmatching/views/ProgramOutput.java
@@ -0,0 +1,16 @@
+package pairmatching.views;
+
+import pairmatching.Course;
+import pairmatching.Level;
+
+import java.util.List;
+
+public class ProgramOutput {
+
+    public void printInitialFeature(){
+        System.out.println("기능을 선택하세요");
+        System.out.print("1. 페어 매칭\n2. 페어 조회\n3.페어 초기화\nQ. 종료\n");
+    }
+
+    
+}
